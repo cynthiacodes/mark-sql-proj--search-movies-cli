12424e5f6eb6236d14a57173784d117b5bdf673d
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-11 09:58:49 +0100

    greeted user using readline-sync

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,8 +1,14 @@
 import { question } from "readline-sync";
 import { Client } from "pg";
 
+const readlineSync = require("readline-sync");
+
+const userName = readlineSync.question("May I have your name? ");
+console.log(`Hi ${userName}`);
+
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
+const client = new Client({ database: "omdb" });
+
 console.log("Welcome to search-movies-cli!");
