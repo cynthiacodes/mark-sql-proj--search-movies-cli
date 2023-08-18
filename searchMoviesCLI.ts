def62ffc14d87d025d42534a31fee460690045a5
cynthiacodes <cjnkstudy01@hotmail.com>
2023-08-18 13:38:31 +0100

    added while true implementation - needs fixing

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,14 +1,46 @@
-import { question } from "readline-sync";
 import { Client } from "pg";
 
 const readlineSync = require("readline-sync");
 
+//As your database is on your local machine, with default port,
+//and default username and password,
+//we only need to specify the (non-default) database name
+async function searchMovie(searchTerm: string) {
+  const client = new Client({ database: "omdb" });
+  await client.connect();
+
+  const dbQuery = "select * from casts_view where movie_name like $1";
+  const searchValue = [`%${searchTerm}%`];
+
+  try {
+    const result = await client.query(dbQuery, searchValue);
+    console.log(result.rows);
+    console.log("no error occurred, results are above.");
+  } catch (e) {
+    console.error(e.stack);
+  } finally {
+    console.log("closing connection, whether or not an error occurred.");
+    client.end();
+  }
+  console.log("continuing normally");
+}
+
+console.log("Welcome to search-movies-cli!");
+
 const userName = readlineSync.question("May I have your name? ");
 console.log(`Hi ${userName}`);
 
-//As your database is on your local machine, with default port,
-//and default username and password,
-//we only need to specify the (non-default) database name.
-const client = new Client({ database: "omdb" });
+function getUserInput() {
+  return readlineSync.question("Enter a search string (or 'q' to quit): ");
+}
 
-console.log("Welcome to search-movies-cli!");
+while (true) {
+  const searchInput = getUserInput();
+  if (searchInput.toLowerCase() === "q") {
+    console.log("Quitting...");
+    break;
+  }
+
+  searchMovie(searchInput.toLowerCase());
+  console.log("You entered:", searchInput);
+}
